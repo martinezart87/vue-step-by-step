8d479d15c26973f8f8bc1d945d8c86ae9d3435f3
martinezart87 <93553194+martinezart87@users.noreply.github.com>
2022-09-06 20:50:59 +0200

    Episode 8 Handle a Form Submission

--- js/components/episode_7_assigments.js
+++ js/components/episode_7_assigments.js
@@ -7,6 +7,14 @@ export default {
         <section class="space-y-6">
             <episode_7_assigmentsList :assigments="filters.inProgress" title="In progress"></episode_7_assigmentsList>
             <episode_7_assigmentsList :assigments="filters.completed" title="Completed"></episode_7_assigmentsList>
+            
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input v-model="newAssigment" placeholder="New assimgent..." class="p-2" />
+                    <button type="submit" class="bg-white text-black p-2 border-l">Add</button>
+                </div>
+            </form>
+        
         </section>
     `,
 
@@ -16,7 +24,9 @@ export default {
                 { name: 'Finish project', complete: false, id: 1 },
                 { name: 'Read chapter 4', complete: false, id: 2 },
                 { name: 'Turn in homework', complete: false, id: 3 },
-            ]
+            ],
+
+            newAssigment: ''
         }
     },
 
@@ -37,5 +47,24 @@ export default {
             };
         }
 
+    },
+
+    methods: {
+        // bez refresha
+        // add(e) {
+        //     e.preventDefault();
+        //     alert('hi');
+        // }
+        add() {
+            this.assigments.push({
+                name: this.newAssigment,
+                completed:false,
+                id: this.assigments.length + 1
+            });
+
+            this.newAssigment = '';
+            // alert(this.newAssigment);
+            // document.querySelector
+        }
     }
 }
