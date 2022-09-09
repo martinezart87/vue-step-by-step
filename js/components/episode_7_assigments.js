e2b564bc972ec0f635d1815429d12b5ce2ac0412
martinezart87 <93553194+martinezart87@users.noreply.github.com>
2022-09-09 21:54:56 +0200

    Episode 10

--- js/components/episode_7_assigments.js
+++ js/components/episode_7_assigments.js
@@ -19,9 +19,9 @@ export default {
     data() {
         return {
             assigments: [
-                { name: 'Finish project', complete: false, id: 1 },
-                { name: 'Read chapter 4', complete: false, id: 2 },
-                { name: 'Turn in homework', complete: false, id: 3 },
+                { name: 'Finish project', complete: false, id: 1, tag: 'math' },
+                { name: 'Read chapter 4', complete: false, id: 2, tag: 'sience' },
+                { name: 'Turn in homework', complete: false, id: 3, tag: 'math' },
             ],
         }
     },
--- js/components/episode_7_assigmentsList.js
+++ js/components/episode_7_assigmentsList.js
@@ -4,10 +4,23 @@ export default {
     components: { episode_7_assigment },
     template: `
         <section v-show="assigments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">{{ title }}
+                <span>({{ assigments.length }})</span>
+            </h2>
+            
+            <div class="flex gap-2">
+                <button 
+                    @click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="border rounded py-1 px-1 text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    }"
+                    >{{ tag }}</button>
+            </div>
 
-            <ul>
-                <episode_7_assigment v-for="assigment in assigments" :key="assigment.id" :assigment="assigment">
+            <ul class="mt-6">
+                <episode_7_assigment v-for="assigment in filteredAssigments" :key="assigment.id" :assigment="assigment">
             </ul>
 
             <!-- <pre>
@@ -19,5 +32,28 @@ export default {
     props: {
         assigments: Array,
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all' 
+        };
+    },
+
+    computed: {
+        filteredAssigments(){
+            if(this.currentTag === 'all') {
+                return this.assigments;
+            }
+            return this.assigments.filter(a => a.tag === this.currentTag);
+        },
+
+        tags() {
+            // return ['sience', 'math', 'reading'];
+            // pobieranie z tagów assigments
+            // new Set - usuwa duplikaty
+            // ... - podają wyfiltrowane nazwy
+            return ['all', ...new Set(this.assigments.map(a => a.tag))];
+        }
     }
 }
